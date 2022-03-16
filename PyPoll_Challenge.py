c69febf9307973d14bd2d8078f4ac6fab7895cd7
Waheed Haidaryar <74505893+haidaryar@users.noreply.github.com>
2022-03-16 09:04:05 -0700

    Moved Resources folder and  changed the  code accordingly

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -7,7 +7,7 @@ from http.client import CannotSendRequest
 import os
 from tkinter import N
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Initialize a total vote counter.
